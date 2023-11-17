779396aae5b9bc2193b023424d9a355874d59b87
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-11-17 15:51:27 +0530

    Add files via upload

---
+++ 1.2(Input)Program to print helloworld using new line character.c
@@ -0,0 +1,10 @@
+//Title of program
+//Program to print helloworld using new line character
+//header file
+#include<stdio.h>
+//function heading
+void main()
+{
+//output statement    
+printf("helloworld\nhelloworld");
+}
