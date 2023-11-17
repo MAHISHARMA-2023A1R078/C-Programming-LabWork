0472194d891c32bfcfe7c60aff40af9d5f820f84
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-11-17 15:52:23 +0530

    Add files via upload

---
+++ 1.3(Input)Program to print helloworld using tab character.c
@@ -0,0 +1,10 @@
+//Title of program
+//Program to print helloworld using tab character
+//header files
+#include<stdio.h>
+//function heading
+void main()
+{
+//output statement    
+printf("helloworld\thelloworld");
+}
